4d217fcc87eeb85f7349afe974caea4eb151bd38
“Antony <antonybutts@me.com>
2020-02-10 20:52:26 +0000

    f

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
+#    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    paht('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),    
 ]
